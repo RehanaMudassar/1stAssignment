5a505b88d66c767d1956a515c7b73dd1621560a7
RehanaMudassar <rehana.mudassar@gmail.com>
2024-03-31 05:48:32 +0500

    any comment

---
+++ ex26.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+let alien_color = 'red';
+if (alien_color == 'green')
+    console.log('the player just earned 5 points for shooting the alien.');
+else
+    console.log('the player just earned 10 points.');
